60c4a805fe09e1b51d253c993ff77e6db84b6bd2
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-06-24 18:57:06 +0300

    accordion menu, hamburger menu

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Pages";
+import styles from "./Header.module.css"
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
+    const [styleHamburger, setStyleHamburger] = useState<string>(styles.hamburger)
+    const [styleHorizontalMenu, setStyleHorizontalMenu] = useState<string>(styles.horizontalMenu)
+    const hamburgerHandler = () => {
+        styleHorizontalMenu===styles.horizontalMenu?
+            setStyleHorizontalMenu(`${styles.horizontalMenu} ${styles.activeHorizontalMenu}`)
+            : setStyleHorizontalMenu(styles.horizontalMenu)
+        styleHamburger===styles.hamburger?
+            setStyleHamburger(`${styles.hamburger} ${styles.activeHamburger}`)
+            : setStyleHamburger(styles.hamburger)
+    }
 
+    return (
+        <div className={styles.headerWrapper} onMouseEnter={hamburgerHandler} onMouseLeave={hamburgerHandler}>
+            <a href="#" className={styleHamburger}>
+                <span className={styles.hamburgerRow}></span>
+                <span className={styles.hamburgerRow}></span>
+                <span className={styles.hamburgerRow}></span>
+            </a>
+
+            <nav className={styleHorizontalMenu}>
+                <NavLink className={styles.horizontalMenuItem} to={PATH.PRE_JUNIOR}>Pre-junior</NavLink>
+                <NavLink className={styles.horizontalMenuItem} to={PATH.JUNIOR}>Junior</NavLink>
+                <NavLink className={styles.horizontalMenuItem} to={PATH.JUNIOR_PLUS}>Junior_plus</NavLink>
+            </nav>
         </div>
     )
 }
