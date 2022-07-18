87d27931de1f3e0d3b3ed381013ce3d1ea4b21d1
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-07-18 18:38:54 +0300

    reducers and tests

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,54 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type SortUpAT = {
+    type: 'sort'
+    payload: 'up'
+}
+
+type SortDownAT = {
+    type: 'sort'
+    payload: 'down'
+}
+
+type CheckAT = {
+    type: 'check'
+    payload: number
+}
+
+export const SortAC = (payload: "up" | "down"): SortUpAT | SortDownAT => {
+
+    return {
+        type: 'sort',
+        payload
+    }
+}
+
+export const CheckAC = (payload: number): CheckAT => {
+
+    return {
+        type: 'check',
+        payload
+    }
+}
+
+export type ActionTypes = CheckAT | SortUpAT | SortDownAT
+
+export const homeWorkReducer = (state: UserType[], action: ActionTypes): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
             // need to fix
-            return state
+            const newState = [...state]
+            if (action.payload === "up") {
+                newState.sort((a, b) => a.name.localeCompare(b.name))
+            } else if (action.payload === "down") {
+                newState.sort((a, b) => b.name.localeCompare(a.name))
+            }
+            return newState
         }
         case 'check': {
-            // need to fix
-            return state
+            return [...state.filter(u => u.age > action.payload)]
         }
-        default: return state
+        default:
+            return state
     }
 }
