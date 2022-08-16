6ed4154f69140a24a6c5ee037f06d2408fd9a2f2
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-08-16 21:12:25 +0300

    setInterval

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -3,39 +3,49 @@ import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
         // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
+        let secondsCounter = 1
         const id: number = window.setInterval(() => {
-            // setDate
+            console.log(date)
+            if (!timerId) {
+                setDate(new Date())
+            } else {
+                const nextDate = new Date(date.toISOString())
+                nextDate.setSeconds(nextDate.getSeconds()+secondsCounter)
+                secondsCounter++
+                setDate(nextDate)
+            }
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date.toLocaleTimeString() // fix with date
+    const stringDate = date.toLocaleDateString() // fix with date
 
     return (
         <div>
-            <div
+            {Boolean(timerId) && (<div
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
                 {stringTime}
-            </div>
-
+            </div>)
+            }
             {show && (
                 <div>
                     {stringDate}
